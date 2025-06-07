346b054186a0b5eb790512a692e7886bdc0c4885
safi-siddiqui-github <safisiddiqui.work@gmail.com>
2025-06-07 20:58:53 +0500

    about-update

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,105 @@
-import Image from "next/image";
+import CssIcon from "@/components/icons/css-icon";
+import HtmlIcon from "@/components/icons/html-icon";
+import JavascriptIcon from "@/components/icons/javascript-icon";
+import NextIcon from "@/components/icons/next-icon";
+import ReactIcon from "@/components/icons/react-icon";
+import TypescriptIcon from "@/components/icons/typescript-icon";
+import UpworkIcon from "@/components/icons/upwork-icon";
+import WhatsappIcon from "@/components/icons/whatsapp-icon";
+import { Github, Linkedin, Mail } from "lucide-react";
 
-export default function Home() {
+export default function Page() {
+  const accounts = [
+    {
+      title: "Gmail",
+      url: "mailto:safisiddiqui.work@gmail.com",
+      icon: Mail,
+      subTitle: "safisiddiqui.work@gmail.com",
+    },
+    {
+      title: "Github",
+      url: "https://github.com/safi-siddiqui-github",
+      icon: Github,
+      subTitle: "safi-siddiqui-github",
+    },
+    {
+      title: "Linkedin",
+      url: "https://www.linkedin.com/in/safi-siddiqui-70377a283/",
+      icon: Linkedin,
+      subTitle: "safi-siddiqui-70377a283",
+    },
+    {
+      title: "Upwork",
+      url: "https://www.upwork.com/freelancers/~01d057b3173e1e2c63",
+      icon: UpworkIcon,
+      subTitle: "Safi S.",
+    },
+    {
+      title: "Whatsapp",
+      url: "https://wa.me/00923117737230",
+      icon: WhatsappIcon,
+      subTitle: "0311-7737230",
+    },
+  ];
+
+  const skills = [
+    {
+      title: "Html 5",
+      icon: HtmlIcon,
+    },
+    {
+      title: "Css 3",
+      icon: CssIcon,
+    },
+    {
+      title: "JavaScript ES6",
+      icon: JavascriptIcon,
+    },
+    {
+      title: "TypeScript 5",
+      icon: TypescriptIcon,
+    },
+    {
+      title: "React JS/TS",
+      icon: ReactIcon,
+    },
+    {
+      title: "Next JS",
+      icon: NextIcon,
+    },
+  ];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex flex-col gap-6 p-6">
+      <div className="flex flex-col gap-1">
+        <h1 className="text-2xl font-medium">Safi Siddiqui</h1>
+        <h2 className="text-lg">Full Stack Next JS Developer</h2>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className="flex flex-col gap-3 items-start">
+        <p className="text-lg font-medium">Emails / Accounts</p>
+
+        {accounts.map((each, index) => (
+          <div className="flex flex-col gap-1" key={`accounts-${index}`}>
+            <p>{each.title}</p>
+            <a href={each.url} className="flex gap-1 items-center">
+              <each.icon />
+              <span>{each.subTitle}</span>
+            </a>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-col gap-4 items-start">
+        <p className="text-lg font-medium">Skills / Frameworks</p>
+
+        {skills.map((each, index) => (
+          <div className="flex gap-1 items-center" key={`skills-${index}`}>
+            <each.icon />
+            <p>{each.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
